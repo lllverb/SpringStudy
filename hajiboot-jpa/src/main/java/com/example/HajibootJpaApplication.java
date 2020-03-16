470d1ba7eeacecce25f8b0c265bfb26bc3dbbb2c
lllverb <wktkm9orz2@gmail.com>
2020-03-17 02:19:36 +0900

    二章まで

--- hajiboot-jpa/src/main/java/com/example/HajibootJpaApplication.java
+++ hajiboot-jpa/src/main/java/com/example/HajibootJpaApplication.java
@@ -1,10 +1,33 @@
 package com.example;
 
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
-public class HajibootJpaApplication {
+public class HajibootJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	CustomerRepository customerRepository;
+	
+	@Override
+	public void run(String... strings) throws Exception {
+		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + "is created");
+		Pageable pageable = PageRequest.of(0, 3);
+		Page<Customer> page = customerRepository.findAllOrderByName(pageable);
+		System.out.println("1ページのデータ数=" + page.getSize());
+		System.out.println("現在のページ=" + page.getNumber());
+		System.out.println("全ページ数=" + page.getTotalPages());
+		System.out.println("全データ数=" + page.getTotalElements());
+		page.getContent().forEach(System.out::println);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HajibootJpaApplication.class, args);
---
+++ hajiboot-jpa/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers(id, first_name, last_name) VALUES(2, 'Nobita', 'Nobi')
+INSERT INTO customers(id, first_name, last_name) VALUES(3, 'Takeshi', 'Goda')
+INSERT INTO customers(id, first_name, last_name) VALUES(4, 'Suneo', 'Honekawa')
+INSERT INTO customers(id, first_name, last_name) VALUES(5, 'Shizuka', 'Minamoto')
