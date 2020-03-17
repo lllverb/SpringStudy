17cedf98555371354c17f7bee128918ec2419bc9
lllverb <wktkm9orz2@gmail.com>
2020-03-17 19:02:13 +0900

    thymeleaf

--- hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -3,10 +3,11 @@ package com.example.api;
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,8 +19,8 @@ public class CustomerRestController {
 	CustomerService customerService;
 	
 	@GetMapping
-	List<Customer> getCustomers(){
-		List<Customer> customers = customerService.findAll();
+	Page<Customer> getCustomers(@PageableDefault Pageable pageable){
+		Page<Customer> customers = customerService.findAll(pageable);
 		return customers;
 	}
 	
@@ -27,5 +28,21 @@ public class CustomerRestController {
 	Optional<Customer> getCustomer(@PathVariable Integer id) {
 		Optional<Customer> customer = customerService.findById(id);
 		return customer;
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	Customer postCustomer(@RequestBody Customer customer) {
+		return customerService.create(customer);
+	}
+	@PutMapping(path = "{id}")
+	Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+		customer.setId(id);
+		return customerService.update(customer);
+	}
+	@DeleteMapping(path = "{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	void deleteCustomer(@PathVariable Integer id) {
+		customerService.delete(id);
 	}
 }
